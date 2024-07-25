be95af813981075e8aaec6d5d8feca2f24f200e1
ankit-pn <miankitsingh@gmail.com>
2024-07-25 18:33:04 +0530

    feat

--- api/index.py
+++ api/index.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import json
+from typing import List, Dict
 
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+# Load data from x.json
+with open('x.json', 'r') as file:
+    appointment_data = json.load(file)
+
+@app.get("/{time}")
+async def get_appointments(time: str) -> List[Dict[str, str]]:
+    if time in appointment_data:
+        return appointment_data[time]
+    else:
+        return []
